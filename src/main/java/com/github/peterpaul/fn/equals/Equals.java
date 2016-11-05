60ce50dd2d489c898c5c38f3b488e9c759bbc562
Peterpaul Taekele Klein Haneveld <pp.kleinhaneveld@gmail.com>
2016-11-05 20:03:13 +0100

    Simplify

--- src/main/java/com/github/peterpaul/fn/equals/Equals.java
+++ src/main/java/com/github/peterpaul/fn/equals/Equals.java
@@ -6,13 +6,10 @@ import javax.annotation.Nonnull;
 public abstract class Equals {
     @SuppressWarnings("unchecked")
     public static <T> boolean equals(@Nonnull T self, @CheckForNull Object o, @Nonnull EqualsChecker<T> equalsChecker) {
-        if (self == o) {
-            return true;
-        }
-        if (o == null) {
-            return false;
-        }
-        return self.getClass().isAssignableFrom(o.getClass()) && equalsChecker.isEqualTo((T) o);
+        return self == o
+                || o != null
+                && self.getClass().isAssignableFrom(o.getClass())
+                && equalsChecker.isEqualTo((T) o);
     }
 
     public interface EqualsChecker<T> {
