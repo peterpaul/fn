8d20ef6da49d1c8f372748c9e38fa66283e6021a
Peterpaul Taekele Klein Haneveld <pp.kleinhaneveld@gmail.com>
2016-11-09 21:51:17 +0100

    Add more reductions.

--- src/main/java/com/github/peterpaul/fn/Reductions.java
+++ src/main/java/com/github/peterpaul/fn/Reductions.java
@@ -3,6 +3,22 @@ package com.github.peterpaul.fn;
 import javax.annotation.Nonnull;
 
 public class Reductions {
+    public static final Reduction<Boolean> ALL_TRUE = new Reduction<Boolean>() {
+        @Nonnull
+        @Override
+        public Boolean apply(@Nonnull Boolean a, @Nonnull Boolean b) {
+            return a && b;
+        }
+    };
+
+    public static final Reduction<Boolean> ANY_TRUE = new Reduction<Boolean>() {
+        @Nonnull
+        @Override
+        public Boolean apply(@Nonnull Boolean a, @Nonnull Boolean b) {
+            return a || b;
+        }
+    };
+
     public static Reduction<String> join(final String delimiter) {
         return new Reduction<String>() {
             @Nonnull
